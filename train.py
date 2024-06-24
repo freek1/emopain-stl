3cf71b468fbd74a30a9338b204a50120af89ab1e
freek1 <freekhens01@gmail.com>
2024-06-24 20:44:06 +0200

    Finish latency

--- train.py
+++ train.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     l1_cls = 1000 # Size of the layer in the classifier
     l2_cls = 0 # Set to 0 to ignore
     drop_p = 0.5 # Dropout setting
-    encoding_method = "STL" # rate, latency, STL
+    encoding_method = "latency" # rate, latency, STL
     # NOTE: To activate the STL-Stacked, set l1sz (and l2sz) to your liking > 0
     #       To use STL-Vanilla, set l1_sz=l2_sz=0.
     avg_window_sz = 100 # For averaging the spiketrains to use as features for the SVM classifier

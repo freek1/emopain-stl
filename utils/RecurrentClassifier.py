44a7bcf03dc34977732cabe71c7eb5b645834b81
freek1 <freekhens01@gmail.com>
2024-07-01 21:54:24 +0200

    Remove extra hidden layer code

--- utils/RecurrentClassifier.py
+++ utils/RecurrentClassifier.py
@@ -50,44 +50,48 @@ class RecurrentClassifier(torch.nn.Module):
         print("Using Recurrent Classifier: Recurrent LIF Neurons.")
 
         self.num_steps = num_steps
-        self.l2_sz = l2_sz
         
         self.fc1 = torch.nn.Linear(encoder_output_size, l1_sz)
-        self.rlif1 = snn.RLeaky(beta=lif_beta, linear_features=l1_sz)
+        self.rlif1 = snn.RLeaky(beta=lif_beta, linear_features=l1_sz, V=1)
         
-        if l2_sz > 0:
-            self.fc2 = torch.nn.Linear(l1_sz, l2_sz)
-            self.rlif2 = snn.RLeaky(beta=lif_beta, linear_features=l2_sz)
-            self.fc3 = torch.nn.Linear(l2_sz, n_classes)
-            self.rlif3 = snn.RLeaky(beta=lif_beta, linear_features=n_classes, V=1)
-        else:
-            self.fc3 = torch.nn.Linear(l1_sz, n_classes)
-            self.rlif2 = snn.RLeaky(beta=lif_beta, linear_features=n_classes, V=1)
+        # Code for adapting to 2 hidden layers (not used in the paper):
+        # self.l2_sz = l2_sz
+        # if l2_sz > 0:
+        #     self.fc2 = torch.nn.Linear(l1_sz, l2_sz)
+        #     self.rlif2 = snn.RLeaky(beta=lif_beta, linear_features=l2_sz)
+        #     self.fc3 = torch.nn.Linear(l2_sz, n_classes)
+        #     self.rlif3 = snn.RLeaky(beta=lif_beta, linear_features=n_classes, V=1)
+        # else:
+        
+        self.fc2 = torch.nn.Linear(l1_sz, n_classes)
+        self.rlif2 = snn.RLeaky(beta=lif_beta, linear_features=n_classes, V=1)
 
     def forward(self, x):
         spk1, mem1 = self.rlif1.init_rleaky()
         spk_rec, mem_rec = [], []
 
-        if self.l2_sz > 0:
-            spk2, mem2 = self.rlif2.init_rleaky()
-            spk3, mem3 = self.rlif3.init_rleaky()
-            for _ in range(self.num_steps):
-                cur1 = self.fc1(x)
-                spk1, mem1 = self.rlif1(cur1, spk1, mem1)
-                cur2 = self.fc2(spk1)
-                spk2, mem2 = self.rlif2(cur2, spk2, mem2)
-                cur3 = self.fc3(spk2)
-                spk3, mem3 = self.rlif3(cur3, spk3, mem3)
-                spk_rec.append(spk3)
-                mem_rec.append(mem3)
-        else:
-            spk2, mem2 = self.rlif2.init_rleaky()
-            for _ in range(self.num_steps):
-                cur1 = self.fc1(x)
-                spk1, mem1 = self.rlif1(cur1, spk1, mem1)
-                cur3 = self.fc3(spk1)
-                spk2, mem2 = self.rlif2(cur3, spk2, mem2)
-                spk_rec.append(spk2)
-                mem_rec.append(mem2)
+        # Code for adapting to 2 hidden layers (not used in the paper):
+        # if self.l2_sz > 0:
+        #     spk2, mem2 = self.rlif2.init_rleaky()
+        #     spk3, mem3 = self.rlif3.init_rleaky()
+        #     for _ in range(self.num_steps):
+        #         cur1 = self.fc1(x)
+        #         spk1, mem1 = self.rlif1(cur1, spk1, mem1)
+        #         cur2 = self.fc2(spk1)
+        #         spk2, mem2 = self.rlif2(cur2, spk2, mem2)
+        #         cur3 = self.fc3(spk2)
+        #         spk3, mem3 = self.rlif3(cur3, spk3, mem3)
+        #         spk_rec.append(spk3)
+        #         mem_rec.append(mem3)
+        # else:
+        
+        spk2, mem2 = self.rlif2.init_rleaky()
+        for _ in range(self.num_steps):
+            cur1 = self.fc1(x)
+            spk1, mem1 = self.rlif1(cur1, spk1, mem1)
+            cur2 = self.fc2(spk1)
+            spk2, mem2 = self.rlif2(cur2, spk2, mem2)
+            spk_rec.append(spk2)
+            mem_rec.append(mem2)
 
         return torch.stack(spk_rec, dim=0), torch.stack(mem_rec, dim=0)
